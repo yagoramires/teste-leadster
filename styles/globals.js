8c4fb6fc0bfeb2dcd51632e28f51ad1daded6d74
yagoramires <yago.ramiresx@gmail.com>
2023-06-28 15:51:08 -0300

    feat: configuração inicial do projeto

---
+++ styles/globals.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing:border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    ul, ol {
+        list-style: none
+    }
+
+    button {
+        cursor:pointer;
+        background: transparent;
+        border: 0;
+    }
+
+`;
